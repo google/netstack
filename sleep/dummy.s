45fe5dbcdd532f7531c6cd5b66d038dbd33c299f
Anton Gyllenberg <anton@iki.fi>
2018-09-25 00:43:03 +0300

    Make go:linkname work for other architectures
    Add an empty assembly file to make the go:linkname directive
    work on other architectures than those that have .s files
    included.

---
+++ sleep/dummy.s
@@ -0,0 +1,18 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This empty assembly file is here to make the sure there is always an
+// assembly file included also for architectures for which there are no
+// other .s files. This is required to make the //go:linkname directive
+// work.
