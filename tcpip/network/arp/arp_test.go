cdd605f5e2f5158c8d3f8f752cba0a53b0aac4ec
Kevin Krakauer <krakauer@google.com>
2019-02-22 13:33:49 -0800

    Rename ping endpoints to icmp endpoints.
    PiperOrigin-RevId: 235248572

--- tcpip/network/arp/arp_test.go
+++ tcpip/network/arp/arp_test.go
@@ -26,7 +26,7 @@ import (
 	"github.com/google/netstack/tcpip/network/arp"
 	"github.com/google/netstack/tcpip/network/ipv4"
 	"github.com/google/netstack/tcpip/stack"
-	"github.com/google/netstack/tcpip/transport/ping"
+	"github.com/google/netstack/tcpip/transport/icmp"
 )
 
 const (
@@ -43,7 +43,7 @@ type testContext struct {
 }
 
 func newTestContext(t *testing.T) *testContext {
-	s := stack.New([]string{ipv4.ProtocolName, arp.ProtocolName}, []string{ping.ProtocolName4}, stack.Options{})
+	s := stack.New([]string{ipv4.ProtocolName, arp.ProtocolName}, []string{icmp.ProtocolName4}, stack.Options{})
 
 	const defaultMTU = 65536
 	id, linkEP := channel.New(256, defaultMTU, stackLinkAddr)
--- tcpip/network/ipv6/icmp_test.go
+++ tcpip/network/ipv6/icmp_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/google/netstack/tcpip/link/channel"
 	"github.com/google/netstack/tcpip/link/sniffer"
 	"github.com/google/netstack/tcpip/stack"
-	"github.com/google/netstack/tcpip/transport/ping"
+	"github.com/google/netstack/tcpip/transport/icmp"
 	"github.com/google/netstack/waiter"
 )
 
@@ -68,8 +68,8 @@ func (e endpointWithResolutionCapability) Capabilities() stack.LinkEndpointCapab
 func newTestContext(t *testing.T) *testContext {
 	c := &testContext{
 		t:      t,
-		s0:     stack.New([]string{ProtocolName}, []string{ping.ProtocolName6}, stack.Options{}),
-		s1:     stack.New([]string{ProtocolName}, []string{ping.ProtocolName6}, stack.Options{}),
+		s0:     stack.New([]string{ProtocolName}, []string{icmp.ProtocolName6}, stack.Options{}),
+		s1:     stack.New([]string{ProtocolName}, []string{icmp.ProtocolName6}, stack.Options{}),
 		icmpCh: make(chan icmpInfo, 10),
 	}
 
