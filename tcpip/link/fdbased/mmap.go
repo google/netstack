ad4b392a9b206797822374aec67cf318173f854c
Bhasker Hariharan <bhaskerh@google.com>
2019-05-21 15:23:12 -0700

    Refactor fdbased endpoint dispatcher code.
    This is in preparation to support an fdbased endpoint that can read/dispatch
    packets from multiple underlying fds.
    
    Updates #231
    
    PiperOrigin-RevId: 249337074

--- tcpip/link/fdbased/mmap.go
+++ tcpip/link/fdbased/mmap.go
@@ -18,16 +18,8 @@ package fdbased
 
 import "github.com/google/netstack/tcpip"
 
-// Stubbed out versions for non-linux/non-amd64 platforms.
+// Stubbed out version for non-linux/non-amd64 platforms.
 
-func (e *endpoint) setupPacketRXRing() error {
-	return nil
-}
-
-func (e *endpoint) readMMappedPacket() ([]byte, *tcpip.Error) {
+func newPacketMMapDispatcher(fd int, e *endpoint) (linkDispatcher, *tcpip.Error) {
 	return nil, nil
-}
-
-func (e *endpoint) packetMMapDispatch() (bool, *tcpip.Error) {
-	return false, nil
 }
